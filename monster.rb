95988627597689e3cf7e8f028e70cde01a492222
brittany <brittanyntackett88@gmail.com>
2017-06-26 22:27:11 -0400

    print 1-100

--- monster.rb
+++ monster.rb
@@ -1,6 +1,14 @@
-def monster
- 100.times do 
- 	puts "dragon"
-end
-end
-monster
+# def monster
+#  100.times do 
+#  	puts "dragon"
+# end
+# end
+# monster
+
+# def counter
+# 	1.upto(100) do |var| 
+# 		puts var
+# 	end
+# end
+# counter
+
