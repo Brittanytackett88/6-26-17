e7296f97574e4e72f426f6eac9da58732cc2812a
brittany <brittanyntackett88@gmail.com>
2017-06-26 23:12:08 -0400

    mined to 3

--- monster.rb
+++ monster.rb
@@ -13,13 +13,25 @@
 # counter
 
 
- def counterpart2 #starting the function
- 	1.upto(100) do |var| #counting the function up to 100
- 		if var % 2==0     # if variable is divisible by 2 with no remainder
- 		puts "even"     # converts to even
- 	else               
- 		puts var # if not putting word "even" keep the number it is
- 	end
- end
+#  def counterpart2 #starting the function
+#  	1.upto(100) do |var| #counting the function up to 100
+#  		if var % 2==0     # if variable is divisible by 2 with no remainder
+#  		puts "even"     # converts to even
+#  	else               
+#  		puts var # if not putting word "even" keep the number it is
+#  	end
+#  end
+# end
+#  counterpart2
+
+
+def mined3
+	1.upto(100) do |var|
+		if var % 3==0
+			puts "mined"
+		else
+			puts var
+	end
+end	
 end
- counterpart2
+mined3			
